57e02138b021b7af573c999657e2c6f962a5f192
Никита Агафонов <nikmac@MacBook-Pro-Nikita.local>
2022-12-17 09:11:52 +0500

    hash roating changes

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,17 @@
 import "../styles/globals.css"
 import { MoralisProvider } from "react-moralis"
 import { NotificationProvider } from "web3uikit"
+import { HashRouter as Roater, Route, Switch, Link } from "react-router-dom"
 
 function MyApp({ Component, pageProps }) {
     return (
-        <MoralisProvider initializeOnMount={false}>
-            <NotificationProvider>
-                <Component {...pageProps} />
-            </NotificationProvider>
-        </MoralisProvider>
+        <Roater>
+            <MoralisProvider initializeOnMount={false}>
+                <NotificationProvider>
+                    <Component {...pageProps} />
+                </NotificationProvider>
+            </MoralisProvider>
+        </Roater>
     )
 }
 
