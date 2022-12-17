ee742bf81161154c7f18d0e95f9df0c54a4e2e8a
Никита Агафонов <nikmac@MacBook-Pro-Nikita.local>
2022-12-17 09:20:06 +0500

    fleek changes

--- pages/_app.js
+++ pages/_app.js
@@ -1,17 +1,14 @@
 import "../styles/globals.css"
 import { MoralisProvider } from "react-moralis"
 import { NotificationProvider } from "web3uikit"
-import { HashRouter as Roater, Route, Switch, Link } from "react-router-dom"
 
 function MyApp({ Component, pageProps }) {
     return (
-        <Roater>
-            <MoralisProvider initializeOnMount={false}>
-                <NotificationProvider>
-                    <Component {...pageProps} />
-                </NotificationProvider>
-            </MoralisProvider>
-        </Roater>
+        <MoralisProvider initializeOnMount={false}>
+            <NotificationProvider>
+                <Component {...pageProps} />
+            </NotificationProvider>
+        </MoralisProvider>
     )
 }
 
